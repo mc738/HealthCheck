5e7c16e9868fd1eb867b9593aca40c5f61610489
Max Clifford <me@mclifford.dev>
2020-06-08 00:29:19 +0100

    Basic healthcheck created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use sysinfo::{ProcessExt, SystemExt};
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut sys = sysinfo::System::new_all();
+
+    sys.refresh_all();
+    
+
+    for (pid, proc_) in sys.get_processes() {
+        println!("{}:{} => {:?}", pid, proc_.name(), proc_.status());
+    }
+
+
+    for component in sys.get_components() {
+        println!("{:?}", component);
+    }
+
+    for disk in sys.get_disks() {
+        println!("{:?}", disk);
+    }
+
+    println!("total memory: {} KiB", sys.get_total_memory());
+    println!("used memory : {} KiB", sys.get_used_memory());
+    println!("total swap  : {} KiB", sys.get_total_swap());
+    println!("used swap   : {} KiB", sys.get_used_swap());
+
 }
